f3d03052a08030fe2fcbb463b135aea79104e253
ishmam139 <88428989+ishmam139@users.noreply.github.com>
2022-09-19 21:31:23 +0600

    Add files via upload

--- values.js
+++ values.js
@@ -2,8 +2,7 @@ let valueHold = {
   name: 'Muhtasim Ishmam',
   age: 14.5
 }
-
-let ishmam = [1,2,3,4,5,6,7];
+let ishu = [1,2,3,4,5,6];
 
 window.addEventListener('load', function(){
     console.log(valueHold.age);
